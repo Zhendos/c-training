859d5c0a2a68c048cb71cc9b3f6c8b27b40f0c57
Youri <youri_da_bomb@hotmail.com>
2015-11-30 19:21:07 +0100

    Create 1-1.cpp

---
+++ 1-1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*
+	 *
+	 *Declared variables.
+	 *
+	 */
+
+	int note;
+
+	/*
+	*
+	*Actual code
+	*
+	*/
+
+	cout << "Type in the note you have received" << endl;
+	cin >> note;
+
+	if (note == 100)
+	{
+		cout << "Great! you got a maxed note!" << endl;
+
+	}
+
+
+
+	return 0;
+}
